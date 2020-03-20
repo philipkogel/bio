d0db8f8bc762b61cd1923a86eb89befe4cf920dc
philipkogel <philipkogel@yahoo.pl>
2020-03-20 21:13:29 +0100

    Updates

---
+++ precache-manifest.375009f3fe0e69f43b9ea58cd53e70ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75f10efc8518722afdbf916e5528be6",
+    "url": "/bio/index.html"
+  },
+  {
+    "revision": "3bf1f8365e4545d5f182",
+    "url": "/bio/static/css/main.b282be09.chunk.css"
+  },
+  {
+    "revision": "369c1616d40716ed3072",
+    "url": "/bio/static/js/2.22d58167.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/bio/static/js/2.22d58167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf1f8365e4545d5f182",
+    "url": "/bio/static/js/main.6c5b5932.chunk.js"
+  },
+  {
+    "revision": "78a1f5ea3cc67302a442",
+    "url": "/bio/static/js/runtime-main.1b6f2837.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bio/static/media/logo.5d5d9eef.svg"
+  }
+]);
